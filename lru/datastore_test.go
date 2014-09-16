73e92818fe8e104e5bd711fdeb1887a766698679
Juan Batiz-Benet <juan@benet.ai>
2014-09-16 04:40:54 -0700

    lru Datastore

---
+++ lru/datastore_test.go
@@ -0,0 +1,53 @@
+package lru_test
+
+import (
+	"strconv"
+
+	"testing"
+
+	ds "github.com/jbenet/datastore.go"
+	lru "github.com/jbenet/datastore.go/lru"
+	. "launchpad.net/gocheck"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type DSSuite struct{}
+
+var _ = Suite(&DSSuite{})
+
+func (ks *DSSuite) TestBasic(c *C) {
+	var size = 1000
+
+	d, err := lru.NewDatastore(size)
+	c.Check(err, Equals, nil)
+
+	for i := 0; i < size; i++ {
+		err := d.Put(ds.NewKey(strconv.Itoa(i)), i)
+		c.Check(err, Equals, nil)
+	}
+
+	for i := 0; i < size; i++ {
+		j, err := d.Get(ds.NewKey(strconv.Itoa(i)))
+		c.Check(j, Equals, i)
+		c.Check(err, Equals, nil)
+	}
+
+	for i := 0; i < size; i++ {
+		err := d.Put(ds.NewKey(strconv.Itoa(i+size)), i)
+		c.Check(err, Equals, nil)
+	}
+
+	for i := 0; i < size; i++ {
+		j, err := d.Get(ds.NewKey(strconv.Itoa(i)))
+		c.Check(j, Equals, nil)
+		c.Check(err, Equals, ds.ErrNotFound)
+	}
+
+	for i := 0; i < size; i++ {
+		j, err := d.Get(ds.NewKey(strconv.Itoa(i + size)))
+		c.Check(j, Equals, i)
+		c.Check(err, Equals, nil)
+	}
+}
