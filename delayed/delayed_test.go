48b1551d94b7b42d4bd0eee27ed7f8c70424e097
Hector Sanjuan <code@hector.link>
2018-02-15 22:07:19 +0100

    Add "delayed" datastore.
    This is a partial extraction from go-ipfs/thirdparty/datastore2.
    
    I have added a test.

---
+++ delayed/delayed_test.go
@@ -0,0 +1,26 @@
+package delayed
+
+import (
+	"testing"
+	"time"
+
+	datastore "github.com/ipfs/go-datastore"
+	delay "github.com/ipfs/go-ipfs-delay"
+)
+
+func TestDelayed(t *testing.T) {
+	d := New(datastore.NewMapDatastore(), delay.Fixed(time.Second))
+	now := time.Now()
+	k := datastore.NewKey("test")
+	err := d.Put(k, "value")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = d.Get(k)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if time.Since(now) < 2*time.Second {
+		t.Fatal("There should have been a delay of 1 second in put and in get")
+	}
+}
