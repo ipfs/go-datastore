523f3f989b1f817ca1dafc2632e1ddd0b47d3a1e
Drew O'Meara <35852900+drew-512@users.noreply.github.com>
2019-02-06 10:33:51 -0600

    added Query.SeekPrefix

--- query/query.go
+++ query/query.go
@@ -59,6 +59,7 @@ cost of the layer of abstraction.
 */
 type Query struct {
 	Prefix            string   // namespaces the query to results whose keys have Prefix
+	SeekPrefix        string   // if set, an alternate to Prefix that initially positions the query (n/a for some impls)
 	Filters           []Filter // filter results. apply sequentially
 	Orders            []Order  // order results. apply hierarchically
 	Limit             int      // maximum number of results
